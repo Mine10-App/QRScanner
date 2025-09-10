7105a9e3a5bb91229632c3df365a16c5c9d047a6
Mine <air1345@gmail.com>
2025-09-10 13:35:19 +0500

    Create sca.js

---
+++ sca.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDvHKlApQ4wClz-y0ejSG91ahKBWvPCSM0",
+  authDomain: "leeli-37413.firebaseapp.com",
+  projectId: "leeli-37413",
+  storageBucket: "leeli-37413.firebasestorage.app",
+  messagingSenderId: "1055234470933",
+  appId: "1:1055234470933:web:a7c063c08dbb21d94e8a1c",
+  measurementId: "G-LMKVKWY3TB"
+};
