742c6555684e06ee6bcbc3c6ce809d2a3509355e
Mine <air1345@gmail.com>
2025-09-10 12:00:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 // Sample users
 const users = [
-  { email: "mohamed.sobah@macl.aero", password: "12345", name: "Mohamed Sobah", rcNo: "T-0160" },
-  { email: "mariyam.shiara@macl.aero", password: "12345", name: "Mariyam Shiara", rcNo: "A-7503" },
-  { email: "musrifa.riyaaz@macl.aero", password: "12345", name: "Musrifa Riyaaz", rcNo: "T-0112" },
-  { email: "hussain.didi@macl.aero", password: "12345", name: "Hussain Ahmed", rcNo: "T-0115" },
-  { email: "inaz.maushoog@macl.aero", password: "12345", name: "Mohamed Inaz", rcNo: "A-4206" },
-  { email: "hamdulla.mohamed@macl.aero", password: "12345", name: "Hamdulla Mohamed", rcNo: "A-5209" },
-  { email: "fiyaza.abdulla@macl.aero", password: "12345", name: "Fiyaza Abdulla", rcNo: "1081" },
-  { email: "fazana.ali@macl.aero", password: "12345", name: "Fazana Ali", rcNo: "T-0154" },
-  { email: "h.haneef@macl.aero", password: "12345", name: "Hassan Haneef", rcNo: "A-3464" },
-  { email: "adam.fazeel@macl.aero", password: "12345", name: "Adam Fazeel", rcNo: "A-4210" },
-  { email: "a.almas@macl.aero", password: "12345", name: "Aminath Almas", rcNo: "A-6313" },
-  { email: "fathimath.maisam@macl.aero", password: "12345", name: "Fathimath Maisam", rcNo: "A-7068" },
-  { email: "i.zihan@macl.aero", password: "12345", name: "Ibrahim Zihan", rcNo: "A-70217" },
-  { email: "aishath.shafaq@macl.aero", password: "12345", name: "Aishath Shafaq", rcNo: "A-7751" },
-  { email: "mohamed.mujahid@macl.aero", password: "12345", name: "Mohamed Mujahid", rcNo: "A-8009" },
-  { email: "magrib.ibrahim@macl.aero", password: "12345", name: "Magrib Ibrahim", rcNo: "A-8038" },
-  { email: "ruhushath.ahmed@macl.aero", password: "12345", name: "Ruhushath Ahmed", rcNo: "A-8089" },
-  { email: "mohamed.jeezaan@macl.aero", password: "12345", name: "Mohamed Jeezaan", rcNo: "A-8156" },
-  { email: "aishath.aroosha@macl.aero", password: "12345", name: "Aishath Aroosha", rcNo: "A-8544" },
-  { email: "liushadha.saeed@macl.aero", password: "12345", name: "Liushadha Saeed", rcNo: "A-8551" },
-  { email: "mariyam.zalma@macl.aero", password: "12345", name: "Mariyam Zalma", rcNo: "A-8553" },
-  { email: "hussain.rameez@macl.aero", password: "12345", name: "Hussain Rameez", rcNo: "A-8610" },
-  { email: "fathimath.shimmu@macl.aero", password: "12345", name: "Fathimath Shimmu", rcNo: "A-8917" },
-  { email: "uoola@macl.aero", password: "12345", name: "Aminath Uoola", rcNo: "A-9623" },
-  { email: "nasrulla.mohamed@macl.aero", password: "12345", name: "Nasrulla Mohamed", rcNo: "T-0033" },
-  { email: "ibrahim.easa@macl.aero", password: "12345", name: "Ibrahim Easa", rcNo: "A-4202" },
-  { email: "ibrahim.wisam@macl.aero", password: "12345", name: "Ibrahim Wisam", rcNo: "A-7465" },
-  { email: "haulath.saeed@macl.aero", password: "12345", name: "Haulath Saeed", rcNo: "A-8538" },
-  { email: "fazleena@macl.aero", password: "12345", name: "Fathmath Fazleena", rcNo: "A-9476" },
-  { email: "nasma@macl.aero", password: "12345", name: "Nasma Ahmed", rcNo: "A-9480" },
-  { email: "saffana@macl.aero", password: "12345", name: "Saffana Abdulla", rcNo: "A-9481" },
-  { email: "abdulla.irufan@macl.aero", password: "12345", name: "Abdulla Irufan", rcNo: "979" },
-  { email: "mohamed.i@macl.aero", password: "12345", name: "Mohamed Ibrahim", rcNo: "623" }
+  { username: "Sobah", password: "12345", name: "Mohamed Sobah", rcNo: "T-0160" },
+  { username: "Shiara", password: "12345", name: "Mariyam Shiara", rcNo: "A-7503" },
+  { username: "musrifa", password: "12345", name: "Musrifa Riyaaz", rcNo: "T-0112" },
+  { username: "hussain", password: "12345", name: "Hussain Ahmed", rcNo: "T-0115" },
+  { username: "inaz", password: "12345", name: "Mohamed Inaz", rcNo: "A-4206" },
+  { username: "hamdulla", password: "12345", name: "Hamdulla Mohamed", rcNo: "A-5209" },
+  { username: "fiyaza", password: "12345", name: "Fiyaza Abdulla", rcNo: "1081" },
+  { username: "fazana", password: "12345", name: "Fazana Ali", rcNo: "T-0154" },
+  { username: "haneef", password: "12345", name: "Hassan Haneef", rcNo: "A-3464" },
+  { username: "fazeel", password: "12345", name: "Adam Fazeel", rcNo: "A-4210" },
+  { username: "almas", password: "12345", name: "Aminath Almas", rcNo: "A-6313" },
+  { username: "maisam", password: "12345", name: "Fathimath Maisam", rcNo: "A-7068" },
+  { username: "zihan", password: "12345", name: "Ibrahim Zihan", rcNo: "A-70217" },
+  { username: "shafaq", password: "12345", name: "Aishath Shafaq", rcNo: "A-7751" },
+  { username: "mujahid", password: "12345", name: "Mohamed Mujahid", rcNo: "A-8009" },
+  { username: "magrib", password: "12345", name: "Magrib Ibrahim", rcNo: "A-8038" },
+  { username: "ruhushath", password: "12345", name: "Ruhushath Ahmed", rcNo: "A-8089" },
+  { username: "jeezaan", password: "12345", name: "Mohamed Jeezaan", rcNo: "A-8156" },
+  { username: "aroosha", password: "12345", name: "Aishath Aroosha", rcNo: "A-8544" },
+  { username: "liushadha", password: "12345", name: "Liushadha Saeed", rcNo: "A-8551" },
+  { username: "zalma", password: "12345", name: "Mariyam Zalma", rcNo: "A-8553" },
+  { username: "rameez", password: "12345", name: "Hussain Rameez", rcNo: "A-8610" },
+  { username: "shimmu", password: "12345", name: "Fathimath Shimmu", rcNo: "A-8917" },
+  { username: "uoola", password: "12345", name: "Aminath Uoola", rcNo: "A-9623" },
+  { username: "nasrulla", password: "12345", name: "Nasrulla Mohamed", rcNo: "T-0033" },
+  { username: "ibrahim", password: "12345", name: "Ibrahim Easa", rcNo: "A-4202" },
+  { username: "wisam", password: "12345", name: "Ibrahim Wisam", rcNo: "A-7465" },
+  { username: "haulath", password: "12345", name: "Haulath Saeed", rcNo: "A-8538" },
+  { username: "fazleena", password: "12345", name: "Fathmath Fazleena", rcNo: "A-9476" },
+  { username: "nasma", password: "12345", name: "Nasma Ahmed", rcNo: "A-9480" },
+  { username: "saffana", password: "12345", name: "Saffana Abdulla", rcNo: "A-9481" },
+  { username: "irufan", password: "12345", name: "Abdulla Irufan", rcNo: "979" },
+  { username: "mohamed", password: "12345", name: "Mohamed Ibrahim", rcNo: "623" }
 ];
 
 function login() {
@@ -49,5 +49,6 @@ function login() {
     document.getElementById("errorMsg").innerText = "Invalid email or password!";
   }
 }
+
 
 
