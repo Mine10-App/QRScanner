b1f6530aee73eec1b6db3d32700a0df24bd3c2a2
Mine <air1345@gmail.com>
2025-09-17 13:46:17 +0500

    Update rate.js

--- rate.js
+++ rate.js
@@ -1,13 +1,8 @@
-const rates = [
-  { AdultA: "37.80", GST: "8%", WP: "Leeli" },
-  { KidsA: "25", GST: "8%",  WP:"A" "Leeli" },
-    { AdultB: "37.80", GST: "8%",  WP: "Leeli" },
-  { KidsB: "25", GST: "8%", WP:"Leeli" }
- 
-
-];
-
-
-
+const rates = {
+  AdultA: { price: 37.80, GST: 8, WP: "Leeli" },
+  KidsA: { price: 25.00, GST: 8, WP: "Leeli" },
+  AdultB: { price: 37.80, GST: 8, WP: "Leeli" },
+  KidsB: { price: 25.00, GST: 8, WP: "Leeli" }
+};
 
 
