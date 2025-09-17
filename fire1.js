402df60c090b434829cd90f390389519502cf0d3
Mine <air1345@gmail.com>
2025-09-17 21:55:02 +0500

    Update fire1.js

--- fire1.js
+++ fire1.js
@@ -1,12 +1,13 @@
 // Your existing Firebase config
-const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
-};
+ const firebaseConfig = {
+    apiKey: "AIzaSyC5G36djlzBuhNEzKzrvcJ_1-qvrTm1bOs",
+    authDomain: "qr-scanner-live-5b385.firebaseapp.com",
+    projectId: "qr-scanner-live-5b385",
+    storageBucket: "qr-scanner-live-5b385.firebasestorage.app",
+    messagingSenderId: "260207453177",
+    appId: "1:260207453177:web:ea438ade7b1b56270e1328",
+    measurementId: "G-WQEEHEPWG3"
+  };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
