1ebaffef50e74a77e49d6e2722e8209ca107c15e
Mine <air1345@gmail.com>
2025-09-17 14:40:39 +0500

    Create fire.js

---
+++ fire.js
@@ -0,0 +1,10 @@
+// ===== Firebase config =====
+  const firebaseConfig = {
+    apiKey: "AIzaSyC5G36djlzBuhNEzKzrvcJ_1-qvrTm1bOs",
+    authDomain: "qr-scanner-live-5b385.firebaseapp.com",
+    projectId: "qr-scanner-live-5b385",
+    storageBucket: "qr-scanner-live-5b385.firebasestorage.app",
+    messagingSenderId: "260207453177",
+    appId: "1:260207453177:web:ea438ade7b1b56270e1328",
+    measurementId: "G-WQEEHEPWG3"
+  };
