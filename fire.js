26e9bfe1edf2ffa248865347be4688df053ac269
Mine <air1345@gmail.com>
2025-09-17 15:04:40 +0500

    Update fire.js

--- fire.js
+++ fire.js
@@ -1,4 +1,7 @@
 // ===== Firebase config =====
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
   const firebaseConfig = {
     apiKey: "AIzaSyC5G36djlzBuhNEzKzrvcJ_1-qvrTm1bOs",
     authDomain: "qr-scanner-live-5b385.firebaseapp.com",
@@ -8,3 +11,5 @@
     appId: "1:260207453177:web:ea438ade7b1b56270e1328",
     measurementId: "G-WQEEHEPWG3"
   };
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
