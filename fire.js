049c9a817d14b119da01a1de76e9109ca68d04e4
Mine <air1345@gmail.com>
2025-09-17 21:32:02 +0500

    Update fire.js

--- fire.js
+++ fire.js
@@ -1,6 +1,4 @@
 // ===== Firebase config =====
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
   const firebaseConfig = {
     apiKey: "AIzaSyC5G36djlzBuhNEzKzrvcJ_1-qvrTm1bOs",
@@ -12,8 +10,4 @@ import { getFirestore, collection, addDoc } from "https://www.gstatic.com/fireba
     measurementId: "G-WQEEHEPWG3"
   };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
-// Export Firestore and addDoc for use in main script
-export { db, collection, addDoc };
