ac5f26b10e4308db878033511ad1d6f0d4d00ad7
Mine <air1345@gmail.com>
2025-09-17 13:33:48 +0500

    Create rate.js

---
+++ rate.js
@@ -0,0 +1,13 @@
+const users = [
+  { AdultA: "37.80", GST: "8%", WP: "Leeli" },
+  { KidsA: "25", GST: "8%",  WP:"A" "Leeli" },
+    { AdultB: "37.80", GST: "8%",  WP: "Leeli" },
+  { KidsB: "25", GST: "8%", WP:"Leeli" }
+ 
+
+];
+
+
+
+
+
